79a46a421b84711035c477d876b0ac61c0b32342
monaja <frmonaja95@gmail.com>
2018-09-29 11:51:54 +0300

    added style and mock-heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +9,12 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+   heroes = HEROES;
+  selectedHero: Hero;
 
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
